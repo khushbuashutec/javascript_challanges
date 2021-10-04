161a1eef0b5b0f00ef587d269c72af377a52a34e
khushbu yadav <khushbu@ashutec.com>
2021-10-04 11:55:16 +0530

    challange 5 of datastructure

--- datastructure_challanges.js
+++ datastructure_challanges.js
@@ -88,55 +88,76 @@ const game = {
 // }
 
 
-//challange 3:
+// //challange 3:
 
-const gameEvents = new Map([
-    [17, '⚽️ GOAL'],
-    [36, '🔁 Substitution'],
-    [47, '⚽️ GOAL'],
-    [61, '🔁 Substitution'],
-    [64, '🔶 Yellow card'],
-    [69, '🔴 Red card'],
-    [70, '🔁 Substitution'],
-    [72, '🔁 Substitution'],
-    [76, '⚽️ GOAL'],
-    [80, '⚽️ GOAL'],
-    [92, '🔶 Yellow card'],
-]);
+// const gameEvents = new Map([
+//     [17, '⚽️ GOAL'],
+//     [36, '🔁 Substitution'],
+//     [47, '⚽️ GOAL'],
+//     [61, '🔁 Substitution'],
+//     [64, '🔶 Yellow card'],
+//     [69, '🔴 Red card'],
+//     [70, '🔁 Substitution'],
+//     [72, '🔁 Substitution'],
+//     [76, '⚽️ GOAL'],
+//     [80, '⚽️ GOAL'],
+//     [92, '🔶 Yellow card'],
+// ]);
 
 
-const events = [...new Set(gameEvents.values())];
+// const events = [...new Set(gameEvents.values())];
 
-console.log(events);
+// console.log(events);
 
-gameEvents.delete(64);
+// gameEvents.delete(64);
 
-console.log(`A event happened on average every ${90 / gameEvents.size} Minutes`);
-const time = [...gameEvents.keys()].pop();
-console.log(time);
+// console.log(`A event happened on average every ${90 / gameEvents.size} Minutes`);
+// const time = [...gameEvents.keys()].pop();
+// console.log(time);
 
-console.log(`A event happened on average every ${time / gameEvents.size} Minutes`);
-for (const [min, event] of gameEvents) {
-    const half = min <= 45 ? 'first' : 'second';
-    console.log(`[${half} half] ${min} : ${event}`);
+// console.log(`A event happened on average every ${time / gameEvents.size} Minutes`);
+// for (const [min, event] of gameEvents) {
+//     const half = min <= 45 ? 'first' : 'second';
+//     console.log(`[${half} half] ${min} : ${event}`);
+// }
+
+// //challange 4
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+
+// document.querySelector('button').addEventListener('click', function () {
+//     const text = document.querySelector('textarea').value;
+
+//     const entry = text.split('\n');
+//     let newEntries = [];
+//     for (const [i, e] of entry.entries()) {
+//         const [first, second] = e.toLowerCase().trim().split('_');
+//         const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+
+//         console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+
+//     }
+
+
+// });
+
+//define one array ane make two different function which return addition and multiplication of array
+const a = [1, 2, 3, 4, 6];
+let sum = 0, multiply = 1;
+function add(x) {
+    const b = x.entries()
+    for (let [i, value] of b) {
+        sum += value;
+    }
+    console.log(`sum of array = ${sum}`);
+}
+function mul(x) {
+    const b = x.entries()
+    for (let [i, value] of b) {
+        multiply *= value;
+    }
+    console.log(`Multiplication of array = ${multiply}`);
 }
 
-//challange 4
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
-
-document.querySelector('button').addEventListener('click', function () {
-    const text = document.querySelector('textarea').value;
-
-    const entry = text.split('\n');
-    let newEntries = [];
-    for (const [i, e] of entry.entries()) {
-        const [first, second] = e.toLowerCase().trim().split('_');
-        const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
-
-        console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
-
-    }
-
-
-});
+add(a)
+mul(a)
