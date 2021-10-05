741fa62191adc57fb6f39e82e9d3f5199ccab6e5
khushbu yadav <khushbu@ashutec.com>
2021-10-05 15:06:05 +0530

    challage for closure function

--- function_challange.js
+++ function_challange.js
@@ -1,15 +1,42 @@
 'use strict'
 
 
+//challange
+
 const poll = {
     question: 'what is your favorite programming language?',
     options: ['0:Javascript', '1:Python', '2:Rust', '3:C++'],
-    answer: new Array(4).fill(0),
+    answers: new Array(4).fill(0),
     registerNewAnswer: function () {
-        const number = prompt(`${this.question.toString()}
-         ${promptData(this.options)}`)
-        console.log(number);
-    }
+        const answer = Number(prompt(`${this.question}\n ${this.options.join('\n')}\n(Write Option Number)`));
+
+        typeof answer === 'number' && answer <= this.answers.length && this.answers[answer]++;
+        console.log(this.answers);
+        this.displayResults();
+        this.displayResults('string');
+
+    },
+    displayResults(type = 'array') {
+        if (typeof type === 'array') {
+            console.log(this.answers);
+        }
+        else {
+            (typeof type === 'string')
+            console.log(`poll results are ${this.answers.join(', ')}`);
+        }
+    },
+
+
 };
-//poll.registerNewAnswer();
-console.log(promptData(poll.options));
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll))
+poll.displayResults.call({ answers: [3, 4, 5] }, 'string');
+
+//challange 2
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+
+    document.querySelector('body').addEventListener('click', function () {
+        header.style.color = 'blue';
+    });
+})();
